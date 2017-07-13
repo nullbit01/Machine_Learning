41f0622c93e15b4ec2f322de11ddfc0548112c8d
Stefanoiu Alexandru <alexstefanoiu@gmail.com>
2017-07-13 17:51:53 +0300

    Perceptron
    Python

---
+++ 02 - Learning and Implementing/Perceptron/Perceptron.py
@@ -0,0 +1,129 @@
+# Perceptron Algorithm
+
+# Imports
+
+from random import seed
+from random import randrange
+from csv import reader
+
+# Load file
+
+def load_csv(filename):
+    dataset = list()
+    with open(filename) as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            if not row:
+                continue
+            dataset.append(row)
+    return dataset
+
+# str to float
+
+def str_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+# str to int
+
+def str_to_int(dataset, column):
+    class_values = [row[column] for row in dataset]
+    unique = set(class_values)
+    lookup = dict()
+    for i, value in enumerate(unique):
+        lookup[value] = i
+    for row in dataset:
+        row[column] = lookup[row[column]]
+    return lookup
+
+# Split in k-folds
+
+def cross_validation(dataset, n_folds):
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+    for i in range(n_folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    return dataset_split
+
+# Calculate accuracy percentage
+
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+# Evaluate an algorithm with cross val
+
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+    folds = cross_validation(dataset, n_folds)
+    scores = list()
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            row_copy[-1] = None
+        predicted = algorithm(train_set, test_set, *args)
+        actual = [row[-1] for row in fold]
+        accuracy = accuracy_metric(actual, predicted)
+        scores.append(accuracy)
+    return scores
+
+# Make predictions with weights
+
+def predict(row, weights):
+    activation = weights[0]
+    for i in range(len(row) - 1):
+        activation += weights[i + 1] * row[i]
+    return 1.0 if activation >= 0.0 else 0.0
+
+# Estimate perceptron using SGD
+
+def train_weights(train, l_rate, n_epoch):
+    weights = [0.0 for i in range(len(train[0]))]
+    for epoch in range(n_epoch):
+        for row in train:
+            prediction = predict(row, weights)
+            error = row[-1] - prediction
+            weights[0] = weights[0] + l_rate * error
+            for i in range(len(row) - 1):
+                weights[i + 1] = weights[i + 1] + l_rate * error * row[i]
+    return weights
+
+# Preceptron algorithm
+
+def perceptron(train, test, l_rate, n_epoch):
+    predictions = list()
+    weights = train_weights(train, l_rate, n_epoch)
+    for row in test:
+        prediction = predict(row, weights)
+        predictions.append(prediction)
+    return predictions
+
+# Main
+
+seed(1)
+
+filename = 'sonar.all-data.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0]) - 1):
+    str_to_float(dataset, i)
+str_to_int(dataset, len(dataset[0]) - 1)
+
+n_folds = 3
+l_rate = 0.01
+n_epoch = 500
+
+scores = evaluate_algorithm(dataset, perceptron, n_folds, l_rate, n_epoch)
+print('Scores: %s' % scores)
+print('Mean Accuracy: %.3f%%' % (sum(scores) / float(len(scores))))
